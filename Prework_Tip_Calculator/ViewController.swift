2b2d5af9d2526068b9c3165ec69f72af94375e9c
alexandrarhodes <atperry30@gmail.com>
2021-08-23 17:57:32 -0700

    Initial Commit

---
+++ Prework_Tip_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prework_Tip_Calculator
+//
+//  Created by Alexandra Rhodes on 8/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
